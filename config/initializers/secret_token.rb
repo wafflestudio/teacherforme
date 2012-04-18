42e83d1f68f72ebd242a9b966747223c0d55c7ec
gin <jineok12@gmail.com>
2012-04-18 17:47:53 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teacherforme::Application.config.secret_token = '56e2a39ffcf6b49dfb2c983385ffa3bc2d8d5151706abcf6e45a6ddff1f35ccf08105893e955434d5d628661d716d0eeb3448d13e5db9ef00d8f70b462143289'
