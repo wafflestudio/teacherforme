96c317923433ea437716455b2d833ebb0e0f3cc9
gin1231 <jineok12@gmail.com>
2012-04-18 23:34:56 +0900

    main controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Teacherforme::Application.routes.draw do
+  get "main/index"
+
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
 
   devise_for :users
@@ -56,7 +58,7 @@ Teacherforme::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'main#index'
 
   # See how all your routes lay out with "rake routes"
 
